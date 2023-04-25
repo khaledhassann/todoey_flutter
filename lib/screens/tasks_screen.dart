af2dbb85b575349cd5baef30f3c6a1556ce59a8d
khaledhassann <kmhtaha@gmail.com>
2023-04-25 18:47:41 +0200

    Initial commit and UI completed design

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/tasks_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  List<String> items = [
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+    'item 5',
+    'item 6',
+    'item 7',
+    'item 8',
+    'item 9',
+    'item 10'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //TODO: Bottom drawer to add new task
+        },
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0,),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0)),
+              ),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+/*items
+                    .map((text) => CheckboxListTile(
+                          value: false,
+                          onChanged: (val) {},
+                          title: Text(text),
+                        ))
+                    .toList(),*/
